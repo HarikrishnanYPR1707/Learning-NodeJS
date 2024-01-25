4b70057b0ed92b6d94b305b64d67f3f43aa2abb0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-25 18:19:42 +0530

    completed with defalut module.exports

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-console.log("Hey there! I am JS");
+// const math = require("./math");
+const { add, sub } = require("./math");
+
+console.log("Addition value is : ", add(123, 432));
+console.log("Substraction value is : ", sub(123, 432));
