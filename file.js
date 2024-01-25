8bb925d34f4eb868cedd3e3a0a33121e6a1e932f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-25 18:39:37 +0530

    file handling - reading a file using readFile

--- file.js
+++ file.js
@@ -7,5 +7,13 @@ const fs = require("fs");
 // Asynchronous Call
 // fs.writeFile("./text.txt", "Hey There this is async", (err) => {});
 
-const result = fs.readFileSync("./contacts.txt", "utf-8");
-console.log(result);
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
