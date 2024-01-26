8525e412cab8d46012e1132341e1d3eef9b24a6f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-26 21:14:48 +0530

    Added res for different route

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const fs = require("fs");
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()} : ${req.url} : New Req Received\n`;
     fs.appendFile("log.txt", log, (err, data) => {
-        res.end("Hello From Server");
+        switch(req.url) {
+            case "/":
+                res.end("Home Page");
+                break;
+            case "/about":
+                res.end("Hii, I am Harikrishnan Prasannakumar");
+                break;
+            case "/contact-me":
+                res.end("Contact me at hari@example.com");
+                break;
+            default:
+                res.end("404 Not Found");
+        }
     });
 });
 
