53fb1ed6d604759571a463463466362795b5da99
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-01-26 19:45:52 +0530

    Deleted files

--- hello.js
+++
@@ -1,5 +0,0 @@
-// const math = require("./math");
-const { add, sub } = require("./math");
-
-console.log("Addition value is : ", add(123, 432));
-console.log("Substraction value is : ", sub(123, 432));
