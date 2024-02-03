fe91fc9f14bd5ee54475b1459fd40e9c4028db6d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-03 22:44:53 +0530

    Added Changes

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()} : ${req.url} : New Req Received\n`;
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch(req.url) {
-            case "/":
-                res.end("Home Page");
-                break;
-            case "/about":
-                res.end("Hii, I am Harikrishnan Prasannakumar");
-                break;
-            case "/contact-me":
-                res.end("Contact me at hari@example.com");
-                break;
-            default:
-                res.end("404 Not Found");
-        }
-    });
+  const log = `${Date.now()} : ${req.url} : New Req Received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("Hii, I am Harikrishnan Prasannakumar");
+        break;
+      case "/contact-me":
+        res.end("Contact me at hari@example.com");
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  });
 });
 
-myServer.listen(8000, () => console.log("Sever Started!!")
-);
+myServer.listen(8000, () => console.log("Sever Started!!"));
+
+// lkjasdf
